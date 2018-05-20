199fba821ff210993b32bb7d6d6501d7b75da068
Bianca Sozzi <biancasozzi@gmail.com>
2018-05-19 21:03:24 -0300

    Link between activities done

--- app/src/main/java/com/bianca/adivinaelnumero/GameActivity.java
+++ app/src/main/java/com/bianca/adivinaelnumero/GameActivity.java
@@ -1,13 +1,41 @@
 package com.bianca.adivinaelnumero;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class GameActivity extends AppCompatActivity {
 
+
+    int game_option;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        SharedPreferences preferences = getSharedPreferences("sharedPreferences", MODE_PRIVATE);
+        game_option = preferences.getInt("game_option", 0);
+        Log.i("game_option", String.valueOf(game_option));
+
+        switch (game_option){
+            case 1:
+                chooseNumber();
+                break;
+            case 2:
+                guessNumber();
+                break;
+            default:
+                Toast.makeText(getApplicationContext(), R.string.error_message_game_option, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void chooseNumber(){
+        Toast.makeText(getApplicationContext(), "MODO 1 - ELEGIR NUMERO", Toast.LENGTH_SHORT).show();
+    }
+
+    private void guessNumber(){
+        Toast.makeText(getApplicationContext(), "MODO 2 - ADIVINAR NUMERO", Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/bianca/adivinaelnumero/MainActivity.java
+++ app/src/main/java/com/bianca/adivinaelnumero/MainActivity.java
@@ -1,13 +1,57 @@
 package com.bianca.adivinaelnumero;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button guessNum, chooseNum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        loadUIElements();
+        onClick();
     }
+
+    private void onClick(){
+
+        final Intent game = new Intent(getApplicationContext(), GameActivity.class);
+        chooseNum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(game);
+                SharedPreferences preferences = getSharedPreferences("sharedPreferences", MODE_PRIVATE);
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putInt("game_option",1);
+                editor.apply();
+            }
+        });
+
+        guessNum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(game);
+                SharedPreferences preferences = getSharedPreferences("sharedPreferences", MODE_PRIVATE);
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putInt("game_option",2);
+                editor.apply();
+            }
+        });
+    }
+
+    private void loadUIElements(){
+        chooseNum = findViewById(R.id.choose_num_btn);
+        guessNum = findViewById(R.id.guess_num_btn);
+    }
+
+
 }
